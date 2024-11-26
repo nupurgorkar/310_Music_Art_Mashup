afb058d8e42d93315a7678eddc9067af4b70e498
nupur gorkar <nupurgorkar@gmail.com>
2024-11-26 10:50:32 -0800

    calls made

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-import keys
+import json
+from flask import Flask, request, redirect, g, render_template
+import requests
+from urllib.parse import quote
+from keys import HARVARD_KEY, Spotify_client_secret, Spoitfy_client_id
 
 
 ##setup
+app = Flask(__name__)
 
+@app.route('/')
--- keys.py
+++ keys.py
@@ -1,1 +1,4 @@
 HARVARD_KEY = '08bf7367-d7fb-449b-b2ad-c50ed01df385'
+
+Spoitfy_client_id = "5aade22775c14edd984abc4cbb9fb707"
+Spotify_client_secret = "024c090ed9dc49548d6e8f2ece4eba1a"
