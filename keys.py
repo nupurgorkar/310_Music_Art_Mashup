18aebcaaa09865cf22bfe148dd274cbf0f57362c
nupur gorkar <nupurgorkar@gmail.com>
2024-12-06 10:04:58 -0800

    FINAL

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
 HARVARD_KEY = '08bf7367-d7fb-449b-b2ad-c50ed01df385'
 
-Spotify_client_id = "5aade22775c14edd984abc4cbb9fb707"
-Spotify_client_secret = "024c090ed9dc49548d6e8f2ece4eba1a"
+Spotify_client_id = "22665308be7448d5b102202fba2be4aa"
+Spotify_client_secret = "f1024aee41a2485daf49e8477eecca9b"
